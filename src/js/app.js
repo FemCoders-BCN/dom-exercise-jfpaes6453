0d7e71f82b78e4d1947f0b53d4e40db2f9380b77
Gabi <hgpc314@gmail.com>
2024-01-08 21:53:26 +0100

    codigo refactorizado y funcional

--- src/js/app.js
+++ src/js/app.js
@@ -1,36 +1,23 @@
-const cartButton = document.getElementById("button")
-const itemTag = document.querySelector(".tag")
-const imgCard = document.querySelector(".product-image")
+const cartButton = document.getElementById("button");
+const itemTag = document.querySelector(".tag");
+const imgCard = document.querySelector(".product-image");
 
-const redCar = () => {
-    cartButton.style.backgroundColor = "red"
-    itemTag.style.backgroundColor = "red"
-    imgCard.style.backgroundImage = "url('../../public/img/redcar.jpg')"
+function changeCar(color, imageUrl) {
+    cartButton.style.backgroundColor = color;
+    itemTag.style.backgroundColor = color;
+    imgCard.style.backgroundImage = `url('../../public/img/${imageUrl}.jpg')`;
 }
 
-const grayCar = () => {
-    cartButton.style.backgroundColor = "gray"
-    itemTag.style.backgroundColor = "gray"
-    imgCard.style.backgroundImage = "url('../../public/img/graycar.jpg')"
-}
-
-const blackCar = () => {
-    cartButton.style.backgroundColor = "black"
-    itemTag.style.backgroundColor = "black"
-    imgCard.style.backgroundImage = "url('../../public/img/blackcar.jpg')"
-}
-
-//const buttonAlert = () => 
-function buttonAlert(){
-    const txt = document.getElementById("white-button").textContent
-    alert(txt)
+function buttonAlert() {
+    const txt = document.getElementById("white-button").textContent;
+    alert(txt);
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    document.querySelector(".red").addEventListener('click', redCar);
-    document.querySelector(".gray").addEventListener('click', grayCar);
-    document.querySelector(".black").addEventListener('click', blackCar);
+    document.querySelector(".red").addEventListener('click', () => changeCar("red", "redcar"));
+    document.querySelector(".gray").addEventListener('click', () => changeCar("gray", "graycar"));
+    document.querySelector(".black").addEventListener('click', () => changeCar("black", "blackcar"));
     document.getElementById("button").addEventListener('click', buttonAlert);
 });
 
-export {buttonAlert}
+export { buttonAlert };
